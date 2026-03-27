afe8dd5f87a99fcefe9e538e15d42cec7e25cfe4
Mohamed Reda Ez-zouhry <m.reda.ezzouhry@gmail.com>
2026-03-27 04:12:01 +0100

    Update test_bench.sv

--- tb/test_bench.sv
+++ tb/test_bench.sv
@@ -1,18 +1,15 @@
 module test_bench();
   logic clk;
   logic reset_n;
-  integer cycle_count = 0;  // Counter to track the number of clock cycles
+  integer cycle_count = 0;  
+  // Clk
+  always #5 clk = ~clk;  
 
-  // Clock generation
-  always #5 clk = ~clk;  // Clock with a period of 10 time units
-
-  // Instantiate the yarp_top module
   top #(.RESET_PC(32'h0)) dut (
     .clk(clk),
     .reset_n(reset_n)
   );
 
-  // Clock cycle counter to terminate simulation after 400 clock cycles
   always @(posedge clk) begin
     cycle_count <= cycle_count + 1;
     if (cycle_count == 200) begin
@@ -20,19 +17,12 @@ module test_bench();
     end
   end
 
-  // Testbench initialization
+  // initialization
   initial begin
-    // Initialize signals
-    clk = 0;           // Start clock at 0
-    reset_n = 0;       // Active-low reset initially asserted
+    clk = 0;           
+    reset_n = 0;       
     
-    // Apply reset sequence
-    #10 reset_n = 1;   // Deassert reset after 10 time units
+    // reset 
+    #10 reset_n = 1;   
   end
-
- // Run simulation for 2000 time units and finish
- // #50000 $finish;
-
-
-
 endmodule
